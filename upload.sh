2cbacd31a74bcd8eee9b0e43ac988a98fb0b3891
James Tatum <jtatum@gmail.com>
2011-07-10 21:21:25 -0700

    May need to remove some dirs prior to upload

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# If you did a sudo python setup.py install, you may need to remove some
+# directories first - dist, build, and atomac.egg-info
 python setup.py register
 python setup.py sdist upload --sign
 python setup.py bdist_egg upload --sign
