9a353e787b7f42d7b82330417c4ac105b958cd5c
Nagappan Alagappan <nagappan@gmail.com>
2012-09-22 01:00:50 -0700

    Added launchapp API

--- atomac/ldtpd/core.py
+++ atomac/ldtpd/core.py
@@ -71,7 +71,6 @@ class Core(ComboBox, Menu, Mouse, PageTabList, Text, Table, Value, Generic):
         @return: True on success.
         @rtype: boolean
         """
-
         return True
 
     def getobjectlist(self, window_name):
@@ -90,6 +89,36 @@ class Core(ComboBox, Menu, Mouse, PageTabList, Text, Table, Value, Generic):
         window_handle, name, app=self._get_window_handle(window_name)
         object_list=self._get_appmap(window_handle, name)
         return object_list.keys()
+
+    def launchapp(self, cmd, args = [], delay = 0, env = 1, lang = "C"):
+        """
+        Launch application.
+
+        @param cmd: Command line string to execute.
+        @type cmd: string
+        @param args: Arguments to the application
+        @type args: list
+        @param delay: Delay after the application is launched
+        @type delay: int
+        @param env: GNOME accessibility environment to be set or not
+        @type env: int
+        @param lang: Application language to be used
+        @type lang: string
+
+        @return: 1 on success
+        @rtype: integer
+
+        @raise LdtpServerException: When command fails
+        """
+        if atomac.NativeUIElement.launchAppByBundlePath(cmd):
+            # Let us wait so that the application launches
+            try:
+                time.sleep(int(delay))
+            except ValueError:
+                time.sleep(5)
+            return 1
+        else:
+            raise LdtpServerException(u"Unable to find app '%s'" % cmd)
 
     def wait(self, timeout=5):
         """
