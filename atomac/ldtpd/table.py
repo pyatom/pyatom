44d6a097866458275da64e164de164924a51c897
Nagappan Alagappan <nagappan@gmail.com>
2012-11-24 14:11:09 -0800

    Added getcellsize API

--- atomac/ldtpd/table.py
+++ atomac/ldtpd/table.py
@@ -226,6 +226,40 @@ class Table(Utils):
             obj=cell.AXChildren[column]
         return obj.AXValue
 
+    def getcellsize(self, window_name, object_name, row_index, column=0):
+        """
+        Get cell size
+        
+        @param window_name: Window name to type in, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+        @param object_name: Object name to type in, either full name,
+        LDTP's name convention, or a Unix glob. 
+        @type object_name: string
+        @param row_index: Row index to get
+        @type row_index: index
+        @param column: Column index to get, default value 0
+        @type column: index
+
+        @return: cell coordinates on success.
+        @rtype: list
+        """
+        object_handle=self._get_object_handle(window_name, object_name)
+        if not object_handle.AXEnabled:
+            raise LdtpServerException(u"Object %s state disabled" % object_name)
+
+        count=len(object_handle.AXRows)
+        if row_index < 0 or row_index > count:
+            raise LdtpServerException('Row index out of range: %d' % row_index)
+        cell=object_handle.AXRows[row_index]
+        count=len(cell.AXChildren)
+        if column < 0 or column > count:
+            raise LdtpServerException('Column index out of range: %d' % column)
+        obj=cell.AXChildren[column]
+        if not re.search("AXColumn", obj.AXRole):
+            obj=cell.AXChildren[column]
+        return self._getobjectsize(obj)
+
     def rightclick(self, window_name, object_name, row_text):
         """
         Right click on table cell
