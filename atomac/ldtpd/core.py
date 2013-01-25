c550652c127d0e4ba19205cc5f8013a60401d3ee
Nagappan Alagappan <nagappan@gmail.com>
2013-01-25 11:11:53 -0800

    Added new APIs - activatewindow, closewindow, maximizewindow, minimizewindow

--- atomac/ldtpd/core.py
+++ atomac/ldtpd/core.py
@@ -465,6 +465,106 @@ class Core(ComboBox, Menu, Mouse, PageTabList, Text, Table, Value, Generic):
         time.sleep(timeout)
         return 1
 
+    def closewindow(self, window_name):
+        """
+        Close window.
+        
+        @param window_name: Window name to look for, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        """
+        object_name="btnclosebutton"
+        object_handle=self._get_object_handle(window_name, object_name)
+        if not object_handle.AXEnabled:
+            raise LdtpServerException(u"Object %s state disabled" % object_name)
+        size=self._getobjectsize(object_handle)
+        self._grabfocus(object_handle)
+        self.wait(0.5)
+        # If object doesn't support Press, trying clicking with the object
+        # coordinates, where size=(x, y, width, height)
+        # click on center of the widget
+        # Noticed this issue on clicking AXImage
+        # click('Instruments*', 'Automation')
+        # on OSX 10.7 default "b1c" doesn't work
+        # so using "b1d", verified with Fusion test, this works
+        self.generatemouseevent(size[0] + size[2]/2, size[1] + size[3]/2, "b1c")
+        return 1
+
+    def minimizewindow(self, window_name):
+        """
+        Minimize window.
+        
+        @param window_name: Window name to look for, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        """
+        object_name="btnminimizebutton"
+        object_handle=self._get_object_handle(window_name, object_name)
+        if not object_handle.AXEnabled:
+            raise LdtpServerException(u"Object %s state disabled" % object_name)
+        size=self._getobjectsize(object_handle)
+        self._grabfocus(object_handle)
+        self.wait(0.5)
+        # If object doesn't support Press, trying clicking with the object
+        # coordinates, where size=(x, y, width, height)
+        # click on center of the widget
+        # Noticed this issue on clicking AXImage
+        # click('Instruments*', 'Automation')
+        # on OSX 10.7 default "b1c" doesn't work
+        # so using "b1d", verified with Fusion test, this works
+        self.generatemouseevent(size[0] + size[2]/2, size[1] + size[3]/2, "b1c")
+        return 1
+
+    def maximizewindow(self, window_name):
+        """
+        Maximize window.
+        
+        @param window_name: Window name to look for, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        """
+        object_name="btnzoombutton"
+        object_handle=self._get_object_handle(window_name, object_name)
+        if not object_handle.AXEnabled:
+            raise LdtpServerException(u"Object %s state disabled" % object_name)
+        size=self._getobjectsize(object_handle)
+        self._grabfocus(object_handle)
+        self.wait(0.5)
+        # If object doesn't support Press, trying clicking with the object
+        # coordinates, where size=(x, y, width, height)
+        # click on center of the widget
+        # Noticed this issue on clicking AXImage
+        # click('Instruments*', 'Automation')
+        # on OSX 10.7 default "b1c" doesn't work
+        # so using "b1d", verified with Fusion test, this works
+        self.generatemouseevent(size[0] + size[2]/2, size[1] + size[3]/2, "b1c")
+        return 1
+
+    def activatewindow(self, window_name):
+        """
+        Activate window.
+        
+        @param window_name: Window name to look for, either full name,
+        LDTP's name convention, or a Unix glob.
+        @type window_name: string
+
+        @return: 1 on success.
+        @rtype: integer
+        """
+        object_name="btnminimizebutton"
+        object_handle=self._get_object_handle(window_name, object_name)
+        self._grabfocus(object_handle.AXWindow)
+        return 1
+
     def click(self, window_name, object_name):
         """
         Click item.
