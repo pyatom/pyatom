432c8d1cfb89bdd9ece4434ed992306c92f05e56
Nagappan Alagappan <nagappan@gmail.com>
2013-01-25 02:58:37 -0800

    Fixed selectitem and selectindex with popup button widget ex: Fusion->Settings->Advanced->Automatic

--- atomac/ldtpd/combo_box.py
+++ atomac/ldtpd/combo_box.py
@@ -55,12 +55,13 @@ class ComboBox(Utils):
             x, y, width, height=self._getobjectsize(object_handle)
             # Mouse left click on the object
             # Note: x + width/2, y + height / 2 doesn't work
-            object_handle.clickMouseButtonLeft((x + 5, y + 5))
+            self.generatemouseevent(x + 5, y + 5, "b1c")
             self.wait(5)
             handle=self._get_sub_menu_handle(object_handle, item_name)
             x, y, width, height=self._getobjectsize(handle)
-            handle.clickMouseButtonLeft((x + 5, y + 5))
-            self.wait(5)
+            # on OSX 10.7 default "b1c" doesn't work
+            # so using "b1d", verified with Fusion test, this works
+            self.generatemouseevent(x + 5, y + 5, "b1d")
             return 1
         # Required for menuitem to appear in accessibility list
         self.wait(1)
@@ -103,9 +104,18 @@ class ComboBox(Utils):
         object_handle=self._get_object_handle(window_name, object_name)
         if not object_handle.AXEnabled:
             raise LdtpServerException(u"Object %s state disabled" % object_name)
-        object_handle.Press()
+        try:
+            object_handle.Press()
+        except AttributeError:
+            # AXPress doesn't work with Instruments
+            # So did the following work around
+            self._grabfocus(object_handle)
+            x, y, width, height=self._getobjectsize(object_handle)
+            # Mouse left click on the object
+            # Note: x + width/2, y + height / 2 doesn't work
+            self.generatemouseevent(x + 5, y + 5, "b1c")
         # Required for menuitem to appear in accessibility list
-        self.wait(1)
+        self.wait(2)
         if not object_handle.AXChildren:
             raise LdtpServerException(u"Unable to find menu")
         # Get AXMenu
@@ -128,7 +138,32 @@ class ComboBox(Utils):
             menu_handle=children[item_index]
             if not menu_handle.AXEnabled:
                 raise LdtpServerException(u"Object %s state disabled" % menu_list[-1])
-            menu_handle.Press()
+            self._grabfocus(menu_handle)
+            x, y, width, height=self._getobjectsize(menu_handle)
+            print menu_handle, x, y, width, height
+            # on OSX 10.7 default "b1c" doesn't work
+            # so using "b1d", verified with Fusion test, this works
+            window=object_handle.AXWindow
+            # For some reason,
+            # self.generatemouseevent(x + 5, y + 5, "b1d")
+            # doesn't work with Fusion settings
+            # Advanced window, so work around with this
+            # ldtp.selectindex('*Advanced', 'Automatic', 1)
+            """
+            Traceback (most recent call last):
+               File "build/bdist.macosx-10.8-intel/egg/atomac/ldtpd/utils.py", line 178, in _dispatch
+                  return getattr(self, method)(*args)
+               File "build/bdist.macosx-10.8-intel/egg/atomac/ldtpd/combo_box.py", line 146, in selectindex
+                  self.generatemouseevent(x + 5, y + 5, "b1d")
+               File "build/bdist.macosx-10.8-intel/egg/atomac/ldtpd/mouse.py", line 97, in generatemouseevent
+                  window=self._get_front_most_window()
+               File "build/bdist.macosx-10.8-intel/egg/atomac/ldtpd/utils.py", line 185, in _get_front_most_window
+                  front_app=atomac.NativeUIElement.getFrontmostApp()
+               File "build/bdist.macosx-10.8-intel/egg/atomac/AXClasses.py", line 114, in getFrontmostApp
+                  raise ValueError('No GUI application found.')
+            ValueError: No GUI application found.
+            """
+            window.doubleClickMouse((x + 5, y + 5))
             # If menuitem already pressed, set child to None
             # So, it doesn't click back in combobox in finally block
             child=None
