a51cf78ff46276bcd6cc141370c7cb299fa3f5bc
Nagappan Alagappan <nagappan@gmail.com>
2013-01-25 11:09:39 -0800

    Grab focus based on AXWindow handle

--- atomac/ldtpd/combo_box.py
+++ atomac/ldtpd/combo_box.py
@@ -46,12 +46,12 @@ class ComboBox(Utils):
         object_handle=self._get_object_handle(window_name, object_name)
         if not object_handle.AXEnabled:
             raise LdtpServerException(u"Object %s state disabled" % object_name)
+        self._grabfocus(object_handle.AXWindow)
         try:
             object_handle.Press()
         except AttributeError:
             # AXPress doesn't work with Instruments
             # So did the following work around
-            self._grabfocus(object_handle)
             x, y, width, height=self._getobjectsize(object_handle)
             # Mouse left click on the object
             # Note: x + width/2, y + height / 2 doesn't work
@@ -104,12 +104,12 @@ class ComboBox(Utils):
         object_handle=self._get_object_handle(window_name, object_name)
         if not object_handle.AXEnabled:
             raise LdtpServerException(u"Object %s state disabled" % object_name)
+        self._grabfocus(object_handle.AXWindow)
         try:
             object_handle.Press()
         except AttributeError:
             # AXPress doesn't work with Instruments
             # So did the following work around
-            self._grabfocus(object_handle)
             x, y, width, height=self._getobjectsize(object_handle)
             # Mouse left click on the object
             # Note: x + width/2, y + height / 2 doesn't work
