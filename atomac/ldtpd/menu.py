d28888c3eb6e8b497ab45a059182f1e9ad58da92
Nagappan Alagappan <nagappan@gmail.com>
2013-02-07 12:54:40 -0800

    When AXChildren is None, click menu_handle and then wait for AXChildren, if available then get children. At end Cancel, if the menu invoked here

--- atomac/ldtpd/menu.py
+++ atomac/ldtpd/menu.py
@@ -127,18 +127,30 @@ class Menu(Utils):
         @rtype: list
         """
         menu_handle=self._get_menu_handle(window_name, object_name)
-        role, label=self._ldtpize_accessible(menu_handle) 
-        if not menu_handle.AXChildren:
-            raise LdtpServerException(u"Unable to find children under menu %s" % \
-                                      label)
-        children=menu_handle.AXChildren[0]
-        sub_menus=[]
-        for current_menu in children.AXChildren:
-            role, label=self._ldtpize_accessible(current_menu)
-            if not label:
-                # All splitters have empty label
-                continue
-            sub_menus.append(u"%s%s" % (role, label))
+        role, label=self._ldtpize_accessible(menu_handle)
+        menu_clicked=False
+        try:
+            if not menu_handle.AXChildren:
+                menu_clicked=True
+                try:
+                    menu_handle.Press()
+                    self.wait(1)
+                except atomac._a11y.ErrorCannotComplete:
+                    pass
+                if not menu_handle.AXChildren:
+                    raise LdtpServerException(u"Unable to find children under menu %s" % \
+                                                  label)
+            children=menu_handle.AXChildren[0]
+            sub_menus=[]
+            for current_menu in children.AXChildren:
+                role, label=self._ldtpize_accessible(current_menu)
+                if not label:
+                    # All splitters have empty label
+                    continue
+                sub_menus.append(u"%s%s" % (role, label))
+        finally:
+            if menu_clicked:
+                menu_handle.Cancel()
         return sub_menus
 
     def verifymenucheck(self, window_name, object_name):
