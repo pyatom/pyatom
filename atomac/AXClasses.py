8ecdd40f2adc2559e479525d24f1367771338cdf
tomatobrown <tmaderbrown@makemusic.com>
2014-06-09 12:46:29 -0500

    Update AXClasses.py
    Added doubleClickDragMouseButtonLeft and doubleClickDragMouseButtonLeft

--- atomac/AXClasses.py
+++ atomac/AXClasses.py
@@ -914,6 +914,20 @@ class NativeUIElement(BaseAXUIElement):
       self._queueMouseButton(coord, Quartz.kCGMouseButtonLeft, modFlags, dest_coord = dest_coord)
       self._postQueuedEvents(interval = interval)
 
+   def doubleClickDragMouseButtonLeft(self, coord, dest_coord, interval = 0.5):
+      ''' Double-click and drag the left mouse button without modifiers pressed
+
+          Parameters: coordinates to double-click on screen (tuple (x, y))
+                      dest coordinates to drag to (tuple (x, y))
+                      interval to send event of btn down, drag and up
+          Returns: None
+      '''
+      modFlags = 0
+      self._queueMouseButton(coord, Quartz.kCGMouseButtonLeft, modFlags, dest_coord = dest_coord)
+      self._queueMouseButton(coord, Quartz.kCGMouseButtonLeft, modFlags, dest_coord = dest_coord,
+                             clickCount=2)
+      self._postQueuedEvents(interval = interval)
+
    def clickMouseButtonLeft(self, coord, interval=None):
       ''' Click the left mouse button without modifiers pressed
 
@@ -981,6 +995,19 @@ class NativeUIElement(BaseAXUIElement):
       # click
       self._queueMouseButton(coord, Quartz.kCGMouseButtonLeft, modFlags,
                              clickCount=2)
+      self._postQueuedEvents()
+
+   def doubleMouseButtonLeftWithMods(self, coord, modifiers):
+      ''' Click the left mouse button with modifiers pressed
+
+          Parameters: coordinates to click; modifiers (list)
+          Returns: None
+      '''
+      modFlags = self._pressModifiers(modifiers)
+      self._queueMouseButton(coord, Quartz.kCGMouseButtonLeft, modFlags)
+      self._queueMouseButton(coord, Quartz.kCGMouseButtonLeft, modFlags,
+                             clickCount=2)
+      self._releaseModifiers(modifiers)
       self._postQueuedEvents()
 
    def tripleClickMouse(self, coord):
